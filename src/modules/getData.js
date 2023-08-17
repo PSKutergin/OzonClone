2d70505e37a671d24a885df8546569cd790fde8c
PSKutergin <PSKutergin@gmail.com>
2023-08-17 22:31:02 +0300

    add_simple-notify

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,22 @@
+import Notify from 'simple-notify'
+
 const getData = () => {
-  return fetch('https://ozonclone-default-rtdb.firebaseio.com/goods.json')
-    .then((response) => {
+  return fetch('https://ozonclone-default-rtdb.firebaseio.com/goods.json').then((response) => {
+      if (response.ok) {
         return response.json()
+      } else {
+        throw new Error('Ошибка запроса!')
+      }
+    })
+    .catch(error => {
+      new Notify({
+        status: 'error',
+        title: 'Ошибка!',
+        text: error.message,
+        effect: 'slide',
+        type: 3,
+        autoclose: true
+      })
     })
 }
 
